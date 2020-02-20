c312398a74bc4743bbe34576ac19617348cb5bfd
WillChaos <46702070+WillChaos@users.noreply.github.com>
2020-02-20 16:01:49 +0800

    Update MigrateUPN.ps1

--- MigrateUPN.ps1
+++ MigrateUPN.ps1
@@ -18,7 +18,8 @@
 
 .INFO
   Script doesnt handle any real advanced error checking yet. however, it has been stablly used on multiple migrations.
-  Reccomended to target a test OU first.
+  Reccomended to target a test OU first. 
+  (for example: no null or sanity checks on OU/UPN setters - And no try/catch on set-aduser yet)
 #>
 
 # -------------------------------------------------  PreReqs  --------------------------------------------------------- #
